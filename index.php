2017539a0c50fd1040fbe325a43285dc1dbe42c6
Carlos Sirvent <ext-everis@osha.europa.eu>
2017-06-15 16:39:51 +0200

    initial commit desde pc

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('HTTP_HOST', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 define('APP_PORT', intval($_SERVER['SERVER_PORT']) != 80 ? ':' . $_SERVER['SERVER_PORT'] : '');
 define('APP_CONFIG', APP_ROOT . 'config/');
 
-//test csm 15062017
+//test csm 15062017 - 1
 
 /* Drupal bootstrap procedure */ 
 //define('DRUPAL_ROOT', realpath(__DIR__ . '/../../')); 
